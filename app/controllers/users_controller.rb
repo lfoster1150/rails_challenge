ba9f8858694d89757e508255ddf3a0a82143b61d
lfoster1150 <lfoster1150@gmail.com>
2022-02-03 12:11:31 -0600

    GET Single User Functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     users = User.all
     render json: users.to_json(only: [:id, :first_name, :last_name, :email])
   end
+
+  def show
+    user = User.find(params[:id])
+    render json: user.to_json(only: [:id, :first_name, :last_name, :email])
+  end
+
 end
