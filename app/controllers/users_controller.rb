4bd6634c07550573945163f228df09722dd1b3e3
lfoster1150 <lfoster1150@gmail.com>
2022-02-03 15:34:42 -0600

    transferred code to API only version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,27 @@ class UsersController < ApplicationController
     render json: user.to_json(only: [:id, :first_name, :last_name, :email])
   end
 
+  def new
+    user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user.to_json(only: [:id, :first_name, :last_name, :email])
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+    def user_params
+      new_user = params.require(:user).permit(:first_name, :last_name, :email)
+      parsed_data = JSON.parse(new_user)
+      puts("*****************", parsed_data, "**********************")
+      return parsed_data
+    end
+
 end
